2a5c5831a5f96458732186440966118bfa19fa54
Savia Maria Coalco <48923354+Savia10@users.noreply.github.com>
2023-03-13 10:30:14 +0530

    Add files via upload

---
+++ Services/ICVRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Job_Service.Services
+{
+    public interface ICVRepository
+    {
+        ValueTask<dynamic> GetCVData(string commandText);
+        ValueTask<dynamic> AddEmployee(string title, string description, int locationId, int departmentId, DateTime closingDate);
+        ValueTask<dynamic> UpdateEmployee(int id,string title, string description, int locationId, int departmentId, DateTime closingDate);
+
+    }
+}
