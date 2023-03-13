2ee24923b09b70fc5079ad88d30fd047530f6a65
Savia Maria Coalco <48923354+Savia10@users.noreply.github.com>
2023-03-13 10:27:57 +0530

    Add files via upload

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Job_Service.Controllers
+{
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [Route("/error")]
+        public IActionResult Error() {
+            return Problem();
+        }
+    }
+}
