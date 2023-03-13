b5c298eb8c943d0ded3f4b3ec0fb319835b8a106
Savia Maria Coalco <48923354+Savia10@users.noreply.github.com>
2023-03-13 10:32:24 +0530

    Create ViewControllerTests.cs

---
+++ Job_ServiceTests/Controllers/ViewControllerTests.cs
@@ -0,0 +1,121 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework.Internal;
+using Job_Service.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Job_Service.Controllers.Tests
+{
+    [TestClass()]
+    public class ViewControllerTests
+    {
+        [TestMethod()]
+        public void ViewControllerTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetDepartmentsTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetLocationsTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void CreateEmployeeTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void UpdateEmployeeTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ViewControllerTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void GetTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void GetDepartmentsTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void GetLocationsTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void CreateEmployeeTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void UpdateEmployeeTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void ViewControllerTest2()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTest2()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDepartmentsTest2()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetLocationsTest2()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateEmployeeTest2()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateEmployeeTest2()
+        {
+            Assert.Fail();
+        }
+    }
+}
