9d8ec0d7cd9444a9e8620208a97503ee526ce1de
Savia Maria Coalco <48923354+Savia10@users.noreply.github.com>
2023-03-13 10:29:15 +0530

    Add files via upload

---
+++ Models/Error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Job_Service.Models
+{
+    public class Error
+    {
+            public string ErrorMessage { get; set; }
+          
+    }
+}
